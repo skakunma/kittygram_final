3ddf74b83225311daf0e272c45ec05180ce00586
Дмитрий Скакун <skakun2025@mail.ru>
2024-09-04 23:26:47 +0300

    settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'backend', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', 'backend', '127.0.0.1', '84.18.126.184', 'skakun.zapto.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
